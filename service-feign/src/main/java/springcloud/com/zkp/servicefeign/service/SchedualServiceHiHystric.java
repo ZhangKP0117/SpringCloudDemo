f1039438e5cc44b3558734a0888671485b05386b
zhangkunpeng-v <zhangkunpeng-v@rongcloud.cn>
2019-02-13 12:50:10 +0800

    add 断路器（Hystrix）(Finchley版本)

---
+++ service-feign/src/main/java/springcloud/com/zkp/servicefeign/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package springcloud.com.zkp.servicefeign.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
