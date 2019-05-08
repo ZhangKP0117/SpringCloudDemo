9d4e36204c90755f85b00dfc51dc32adedb9a890
pengpeng <zhangkp1006@163.com>
2019-05-08 09:19:10 +0800

    格式修改

--- service-zuul/src/main/java/com/zkp/servicezuul/MyFilter.java
+++ service-zuul/src/main/java/com/zkp/servicezuul/MyFilter.java
@@ -14,13 +14,11 @@ public class MyFilter extends ZuulFilter {
     private static Logger log = LoggerFactory.getLogger(MyFilter.class);
 
     /**
-     *
-     *     filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
-     *         pre：路由之前
-     *         routing：路由之时
-     *         post： 路由之后
-     *         error：发送错误调用
-     *
+     * filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     * pre：路由之前
+     * routing：路由之时
+     * post： 路由之后
+     * error：发送错误调用
      */
     @Override
     public String filterType() {
@@ -29,7 +27,6 @@ public class MyFilter extends ZuulFilter {
 
 
     /**
-     *
      * filterOrder：通过int值来定义过滤器的执行顺序，数值越小优先级越高。
      */
     @Override
@@ -38,7 +35,6 @@ public class MyFilter extends ZuulFilter {
     }
 
     /**
-     *
      * shouldFilter：返回一个boolean类型来判断该过滤器是否要执行。我们可以通过此方法来指定过滤器的有效范围。
      */
     @Override
@@ -48,7 +44,6 @@ public class MyFilter extends ZuulFilter {
 
 
     /**
-     *
      * run：过滤器的具体逻辑。在该方法中，我们可以实现自定义的过滤逻辑，来确定是否要拦截当前的请求，不对其进行后续的路由，或是在请求路由返回结果之后，对处理结果做一些加工等。
      */
     @Override
@@ -57,13 +52,14 @@ public class MyFilter extends ZuulFilter {
         HttpServletRequest request = ctx.getRequest();
         log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
         Object accessToken = request.getParameter("token");
-        if(accessToken == null) {
+        if (accessToken == null) {
             log.warn("token is empty");
             ctx.setSendZuulResponse(false);
             ctx.setResponseStatusCode(401);
             try {
                 ctx.getResponse().getWriter().write("token is empty");
-            }catch (Exception e){}
+            } catch (Exception e) {
+            }
 
             return null;
         }
